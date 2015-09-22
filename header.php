34deaf7c9a4e5ef1dca5531e6d72eb00a310d76e
Shylux <shylux@gmail.com>
2015-09-22 20:19:38 +0200

    Added base site structure.

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+  <title>The Legend of ROOT</title>
+  <meta charset="utf-8">
+  <link rel="shortcut icon" type="image/x-icon" href="static/img/favicon.ico">
+  <link rel="stylesheet" href="static/css/style.css">
+  <script src="static/js/jquery.js"></script>
+</head>
+<body>
+  <header>
+    <h1>The Legend of ROOT</h1>
+    <?php require "login.php" ?>
+  </header>
+  <div id="content">
---
+++ login.php
@@ -0,0 +1,8 @@
+<div id="login">
+  <form method="POST">
+    <input name="username" type="text" placeholder="Username" />
+    <input name="password" type="password" />
+    <input type="submit" value="Login" />
+  </form>
+  <a href="/register.php">Register</a>
+</div>
